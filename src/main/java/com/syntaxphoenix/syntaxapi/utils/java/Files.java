e361164e9503ac1556f7a0104fdb2aec619ab55d
Laura Endreß <laura.endress@syntaxphoenix.com>
2020-02-05 10:45:35 +0100

    Sorting and Lists update

--- src/main/java/com/syntaxphoenix/syntaxapi/utils/java/Files.java
+++ src/main/java/com/syntaxphoenix/syntaxapi/utils/java/Files.java
@@ -3,42 +3,87 @@ package com.syntaxphoenix.syntaxapi.utils.java;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Files {
 
-	public static List<File> listFiles(File directory) {
-		List<File> files = new ArrayList<>();
-		if (directory.isDirectory()) {
-			File[] ff = directory.listFiles();
-			for (int v = 0; v < ff.length; v++) {
-				if (ff[v].isDirectory()) {
-					files.addAll(listFiles(ff[v]));
-				} else {
-					files.add(ff[v]);
+	public static ArrayList<File> listFiles(File directory) {
+		ArrayList<File> list = new ArrayList<>();
+		for (File file : directory.listFiles()) {
+			if (file.isDirectory()) {
+				list.addAll(listFiles(file));
+			} else {
+				list.add(file);
+			}
+		}
+		return list;
+	}
+
+	public static ArrayList<String> listFileNames(File directory, String fileEnd) {
+		ArrayList<String> list = new ArrayList<>();
+		for (File file : directory.listFiles()) {
+			if (file.isDirectory()) {
+				list.addAll(listFileNames(file, fileEnd));
+			} else {
+				if (file.getName().endsWith(fileEnd)) {
+					list.add(file.getName().replace(fileEnd, ""));
 				}
 			}
 		}
-		return files;
+		return list;
 	}
 
-	public static List<File> listFiles(File directory, String fileEnd) {
-		List<File> files = new ArrayList<>();
-		if (directory.isDirectory()) {
-			File[] ff = directory.listFiles();
-			for (int v = 0; v < ff.length; v++) {
-				if (ff[v].isDirectory()) {
-					files.addAll(listFiles(ff[v], fileEnd));
-				} else {
-					if (ff[v].getName().endsWith(fileEnd)) {
-						files.add(ff[v]);
+	public static ArrayList<String> listFileNames(String fileEnd, File... directories) {
+		ArrayList<String> list = new ArrayList<>();
+		for (File file : directories) {
+			list.addAll(listFileNames(file, fileEnd));
+		}
+		return list;
+	}
+
+	public static ArrayList<File> listFiles(File directory, String fileEnd) {
+		ArrayList<File> list = new ArrayList<>();
+		for (File file : directory.listFiles()) {
+			if (file.isDirectory()) {
+				list.addAll(listFiles(file, fileEnd));
+			} else {
+				if (file.getName().endsWith(fileEnd)) {
+					list.add(file);
+				}
+			}
+		}
+		return list;
+	}
+
+	public static ArrayList<File> listFilesContains(File directory, String contains) {
+		ArrayList<File> list = new ArrayList<>();
+		for (File file : directory.listFiles()) {
+			if (file.isDirectory()) {
+				list.addAll(listFiles(file, contains));
+			} else {
+				if (file.getName().contains(contains)) {
+					list.add(file);
+				}
+			}
+		}
+		return list;
+	}
+
+	public static ArrayList<File> listFiles(File directory, String fileEnd, String containsNot) {
+		ArrayList<File> list = new ArrayList<>();
+		for (File file : directory.listFiles()) {
+			if (file.isDirectory()) {
+				list.addAll(listFiles(file, fileEnd, containsNot));
+			} else {
+				if (file.getName().endsWith(fileEnd)) {
+					if (!file.getName().contains(containsNot)) {
+						list.add(file);
 					}
 				}
 			}
 		}
-		return files;
+		return list;
 	}
-
+	
 	public static void zipFileToFolderTime(File file, File folder) {
 		if (!file.exists()) {
 			return;
--- src/main/java/com/syntaxphoenix/syntaxapi/utils/java/Lists.java
+++ src/main/java/com/syntaxphoenix/syntaxapi/utils/java/Lists.java
@@ -1,17 +1,16 @@
 package com.syntaxphoenix.syntaxapi.utils.java;
 
-import java.io.File;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 import java.util.function.Consumer;
 
 public class Lists {
 
 	@SuppressWarnings("unchecked")
 	public static <E> ArrayList<E> asList(E... array) {
-		ArrayList<E> list = new ArrayList<E>();
+		ArrayList<E> list = new ArrayList<>();
 		for (int index = 0; index < array.length; index++) {
 			list.add(array[index]);
 		}
@@ -19,7 +18,7 @@ public class Lists {
 	}
 
 	public static <E> ArrayList<E> asList(Collection<E> collection) {
-		ArrayList<E> output = new ArrayList<E>();
+		ArrayList<E> output = new ArrayList<>();
 		Iterator<E> iterator = collection.iterator();
 		while (iterator.hasNext()) {
 			output.add(iterator.next());
@@ -27,7 +26,7 @@ public class Lists {
 		return output;
 	}
 
-	public static boolean containsEqualsIgnoreCase(String equals, ArrayList<String> list) {
+	public static boolean containsEqualsIgnoreCase(String equals, List<String> list) {
 		if(list.isEmpty()) return false;
 		for (String input : list) {
 			if (input.equalsIgnoreCase(equals)) {
@@ -38,7 +37,7 @@ public class Lists {
 	}
 
 	public static String[] toStringArray(Collection<String[]> collection) {
-		ArrayList<String> output = new ArrayList<>();
+		List<String> output = new ArrayList<>();
 		collection.forEach(new Consumer<String[]>() {
 			@Override
 			public void accept(String[] array) {
@@ -50,7 +49,7 @@ public class Lists {
 		return output.toArray(new String[0]);
 	}
 
-	public static <E> ArrayList<ArrayList<E>> partition(ArrayList<E> list, int amount) {
+	public static <E> ArrayList<ArrayList<E>> partition(List<E> list, int amount) {
 		ArrayList<ArrayList<E>> parition = new ArrayList<>();
 		ArrayList<E> current = new ArrayList<>();
 		for (int index = 0; index < list.size(); index++) {
@@ -67,96 +66,19 @@ public class Lists {
 	}
 
 	@SafeVarargs
-	public static ArrayList<String> merge(ArrayList<String>... lists) {
+	public static ArrayList<String> merge(List<String>... lists) {
 		ArrayList<String> output = new ArrayList<>();
-		for (ArrayList<String> list : lists) {
+		for (List<String> list : lists) {
 			output.addAll(list);
 		}
 		return output;
 	}
-	public static List<File> listFiles(File directory) {
-		List<File> list = new ArrayList<>();
-		for (File file : directory.listFiles()) {
-			if (file.isDirectory()) {
-				list.addAll(listFiles(file));
-			} else {
-				list.add(file);
-			}
-		}
-		return list;
-	}
 
-	public static List<String> listFileNames(File directory, String fileEnd) {
-		List<String> list = new ArrayList<>();
-		for (File file : directory.listFiles()) {
-			if (file.isDirectory()) {
-				list.addAll(listFileNames(file, fileEnd));
-			} else {
-				if (file.getName().endsWith(fileEnd)) {
-					list.add(file.getName().replace(fileEnd, ""));
-				}
-			}
-		}
-		return list;
-	}
-
-	public static List<String> listFileNames(String fileEnd, File... directories) {
-		List<String> list = new ArrayList<>();
-		for (File file : directories) {
-			list.addAll(listFileNames(file, fileEnd));
-		}
-		return list;
-	}
-
-	public static List<File> listFiles(File directory, String fileEnd) {
-		List<File> list = new ArrayList<>();
-		for (File file : directory.listFiles()) {
-			if (file.isDirectory()) {
-				list.addAll(listFiles(file, fileEnd));
-			} else {
-				if (file.getName().endsWith(fileEnd)) {
-					list.add(file);
-				}
-			}
-		}
-		return list;
-	}
-
-	public static List<File> listFilesC(File directory, String contains) {
-		List<File> list = new ArrayList<>();
-		for (File file : directory.listFiles()) {
-			if (file.isDirectory()) {
-				list.addAll(listFiles(file, contains));
-			} else {
-				if (file.getName().contains(contains)) {
-					list.add(file);
-				}
-			}
-		}
-		return list;
-	}
-
-	public static List<File> listFiles(File directory, String fileEnd, String containsNot) {
-		List<File> list = new ArrayList<>();
-		for (File file : directory.listFiles()) {
-			if (file.isDirectory()) {
-				list.addAll(listFiles(file, fileEnd, containsNot));
-			} else {
-				if (file.getName().endsWith(fileEnd)) {
-					if (!file.getName().contains(containsNot)) {
-						list.add(file);
-					}
-				}
-			}
-		}
-		return list;
-	}
-
-	public static <E> ArrayList<E> createWithType(Class<E> sample) {
+	public static <E> List<E> createWithType(Class<E> sample) {
 		return new ArrayList<>();
 	}
 	
-	public static <E> ArrayList<E> createWithType(E sample) {
+	public static <E> List<E> createWithType(E sample) {
 		return new ArrayList<>();
 	}
 
