f9640c5d3ab9e35da9ebe5dd29ce3c7393fbcb9b
Laura Endreß <laura.endress@syntaxphoenix.com>
2020-02-20 09:46:15 +0100

    Fixed property class (shouldn't be abstract)

--- src/main/java/com/syntaxphoenix/syntaxapi/utils/data/Properties.java
+++ src/main/java/com/syntaxphoenix/syntaxapi/utils/data/Properties.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
  * @author Lauriichen
  *
  */
-public abstract class Properties extends Propertyable implements Serializable {
+public class Properties extends Propertyable implements Serializable {
 	
 	private static final long serialVersionUID = 5806508173901340453L;
 	
