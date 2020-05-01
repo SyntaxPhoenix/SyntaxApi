8982beadff84d230b055dbd57d1d744dc50b74eb
Laura Endreß <laura.endress@syntaxphoenix.com>
2020-05-01 11:43:13 +0200

    Build fix

--- src/main/java/com/syntaxphoenix/syntaxapi/command/builder/ConditionBuilder.java
+++ src/main/java/com/syntaxphoenix/syntaxapi/command/builder/ConditionBuilder.java
@@ -8,7 +8,7 @@ public class ConditionBuilder<E extends BaseInfo> {
 
 	private final CommandBuilder<E> command;
 
-	private final HashMap<Condition<E>, CommandAction<E>> conditions = new HashMap<>();
+	//private final HashMap<Condition<E>, CommandAction<E>> conditions = new HashMap<>();
 
 	private Condition<E> condition;
 //	private <> action; ??? --> BuiltCommand?? // more??
@@ -33,16 +33,16 @@ public class ConditionBuilder<E extends BaseInfo> {
 		return condition = new Condition<>();
 	}
 
-	public CommandAction<E> action() {
-		return action = new CommandAction<>();
-	}
+	//public CommandAction<E> action() {
+	//	return action = new CommandAction<>();
+	//}
 
 	public ConditionBuilder<E> add() {
-		if(action == null)
-			return this;
-		conditions.put(condition != null ? condition : new Condition<>(true), action);
+		//if(action == null)
+		//	return this;
+		//conditions.put(condition != null ? condition : new Condition<>(true), action);
 		condition = null;
-		action = null;
+		//action = null;
 		return this;
 	}
 
