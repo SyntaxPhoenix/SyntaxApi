217de829610123c2f610d9b75002e0cd48b319a7
Laura Endreß <laura.endress@syntaxphoenix.com>
2020-05-02 09:21:38 +0200

    Keys class update

--- src/main/java/com/syntaxphoenix/syntaxapi/utils/java/Keys.java
+++ src/main/java/com/syntaxphoenix/syntaxapi/utils/java/Keys.java
@@ -1,78 +1,128 @@
 package com.syntaxphoenix.syntaxapi.utils.java;
 
-
-
-import java.security.SecureRandom;
+import com.syntaxphoenix.syntaxapi.random.NumberGeneratorType;
+import com.syntaxphoenix.syntaxapi.random.RandomNumberGenerator;
+import com.syntaxphoenix.syntaxapi.utils.java.lang.StringBuilder;
 
 public class Keys {
-	
-	private static final String ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	private static final String NUMBERS = "0123456789";
-    private static final SecureRandom RANDOM = new SecureRandom();
 
-    public static final int DEFAULT_KEY_LENGTH = ALPHABET.length();
-    public static final int DEFAULT_INT_LENGTH = ((Integer.MAX_VALUE+"").length() - 1);
-    public static final int DEFAULT_LONG_LENGTH = ((Long.MAX_VALUE+"").length() - 1);
-    
-    /*
-     * 
-     * String
-     * 
-     */
-    
-    public static String generateKey() {
-    	return generateKey(DEFAULT_KEY_LENGTH);
-    }
-	
-	public static String generateKey(Integer length) {
-        StringBuilder builder = new StringBuilder();
-        for (int current = 0; current < length; ++current) {
-            builder.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
-        }
-        return builder.toString();
+	public static final Keys DEFAULT = new Keys();
+
+	public static final String ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	public static final String NUMBERS = "0123456789";
+
+	public static final int DEFAULT_STRING_LENGTH = ALPHABET.length();
+	public static final int DEFAULT_INT_LENGTH = ((Integer.MAX_VALUE + "").length() - 1);
+	public static final int DEFAULT_LONG_LENGTH = ((Long.MAX_VALUE + "").length() - 1);
+
+	/*
+	 * 
+	 */
+
+	private final RandomNumberGenerator RANDOM;
+
+	/*
+	 * 
+	 */
+
+	public Keys() {
+		this(System.currentTimeMillis());
+	}
+
+	public Keys(long seed) {
+		this(NumberGeneratorType.MURMUR.create(seed));
+	}
+
+	public Keys(RandomNumberGenerator generator) {
+		RANDOM = generator;
+	}
+
+	/*
+	 * 
+	 */
+
+	public final String makeKey() {
+		return makeKey(DEFAULT_STRING_LENGTH);
+	}
+
+	public String makeKey(int length) {
+		StringBuilder builder = new StringBuilder();
+		for (int current = 0; current < length; ++current) {
+			builder.append(NUMBERS.charAt(RANDOM.nextInt(NUMBERS.length())));
+		}
+		return builder.toStringClear();
 	}
 	
 	/*
 	 * 
-	 * Integer
-	 * 
 	 */
 	
-	public static Integer generateInt() {
-		return generateInt(DEFAULT_INT_LENGTH);
+	public Integer makeInt() {
+		return makeInt(DEFAULT_INT_LENGTH);
 	}
 	
-	public static Integer generateInt(Integer length) {
-		if(length > DEFAULT_INT_LENGTH) {
-			length = DEFAULT_INT_LENGTH;
+	public Integer makeInt(int length) {
+		StringBuilder builder = new StringBuilder();
+		for(int current = 0; current < length; ++current) {
+			builder.append(NUMBERS.charAt(RANDOM.nextInt(NUMBERS.length())));
 		}
-        StringBuilder builder = new StringBuilder();
-        for (int current = 0; current < length; ++current) {
-            builder.append(NUMBERS.charAt(RANDOM.nextInt(NUMBERS.length())));
-        }
-        return Integer.valueOf(builder.toString());
+		return Integer.valueOf(builder.toStringClear());
 	}
 	
 	/*
 	 * 
-	 * Long
-	 * 
 	 */
 	
-	public static Long generateLong() {
-		return generateLong(DEFAULT_LONG_LENGTH);
+	public Long makeLong() {
+		return makeLong(DEFAULT_LONG_LENGTH);
 	}
 	
-	public static Long generateLong(Integer length) {
-		if(length > DEFAULT_LONG_LENGTH) {
-			length = DEFAULT_LONG_LENGTH;
+	public Long makeLong(int length) {
+		StringBuilder builder = new StringBuilder();
+		for(int current = 0; current < length; ++current) {
+			builder.append(NUMBERS.charAt(RANDOM.nextInt(NUMBERS.length())));
 		}
-        StringBuilder builder = new StringBuilder();
-        for (int current = 0; current < length; ++current) {
-            builder.append(NUMBERS.charAt(RANDOM.nextInt(NUMBERS.length())));
-        }
-        return Long.valueOf(builder.toString());
+		return Long.valueOf(builder.toStringClear());
+	}
+	
+	//
+	//
+	// Static keys stuff
+	// 
+	// To prevent old methods from breaking.
+	//
+	//
+	
+	public static final String generateKey() {
+		return DEFAULT.makeKey();
+	}
+	
+	public static final String generateKey(int length) {
+		return DEFAULT.makeKey(length);
+	}
+	
+	//
+	//
+	//
+	
+	public static final Integer generateInt() {
+		return DEFAULT.makeInt();
+	}
+	
+	public static final Integer generateInt(int length) {
+		return DEFAULT.makeInt(length);
+	}
+	
+	//
+	//
+	//
+	
+	public static final Long generateLong() {
+		return DEFAULT.makeLong();
+	}
+	
+	public static final Long generateLong(int length) {
+		return DEFAULT.makeLong(length);
 	}
 
 }
-
