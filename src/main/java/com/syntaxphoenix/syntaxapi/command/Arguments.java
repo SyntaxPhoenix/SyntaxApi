2820ebbb4384493841f13e165ce5233f7045ca85
Laura Endreß <laura.endress@syntaxphoenix.com>
2020-06-26 13:18:46 +0200

    Added copy method to Arguments

--- src/main/java/com/syntaxphoenix/syntaxapi/command/Arguments.java
+++ src/main/java/com/syntaxphoenix/syntaxapi/command/Arguments.java
@@ -2,6 +2,7 @@ package com.syntaxphoenix.syntaxapi.command;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import com.syntaxphoenix.syntaxapi.command.arguments.ListArgument;
 import com.syntaxphoenix.syntaxapi.exceptions.ObjectLockedException;
@@ -11,119 +12,119 @@ import com.syntaxphoenix.syntaxapi.exceptions.ObjectLockedException;
  *
  */
 public class Arguments implements Iterable<BaseArgument> {
-	
-	private final ArrayList<BaseArgument> arguments;
+
+	private final List<BaseArgument> arguments;
 	private boolean locked = false;
-	
+
 	public Arguments() {
 		this.arguments = new ArrayList<>();
 	}
-	
-	public Arguments(ArrayList<BaseArgument> arguments) {
+
+	public Arguments(List<BaseArgument> arguments) {
 		this.arguments = arguments;
 	}
 
 	public int count() {
 		return arguments.size();
 	}
-	
+
 	public BaseArgument get(int position) {
-		if(position < 1) {
+		if (position < 1) {
 			throw negativeOrZero();
 		}
-		if(position > count()) {
+		if (position > count()) {
 			throw outOfBounce(position);
 		}
 		return arguments.get(position - 1);
 	}
-	
+
 	public void add(BaseArgument argument, int position) {
-		if(locked) {
+		if (locked) {
 			throw locked();
 		}
-		if(position < 1) {
+		if (position < 1) {
 			throw negativeOrZero();
 		}
-		if(argument == null) {
+		if (argument == null) {
 			return;
 		}
 		arguments.add(position - 1, argument);
 	}
-	
+
 	public void add(BaseArgument argument) {
-		if(locked) {
+		if (locked) {
 			throw locked();
 		}
-		if(argument == null) {
+		if (argument == null) {
 			return;
 		}
 		arguments.add(argument);
 	}
-	
+
 	public ArgumentType getType(int position) {
 		return get(position).getType();
 	}
-	
+
 	public ArgumentSuperType getSuperType(int position) {
 		return getType(position).getSuperType();
 	}
-	
+
 	protected boolean isLocked() {
 		return locked;
 	}
-	
+
 	protected void setLocked(boolean locked) {
 		this.locked = locked;
 	}
-	
+
 	/*
-	 * 
+	 * toString
 	 */
-	
+
 	@Override
 	public String toString() {
 		return toString(ArgumentSerializer.DEFAULT);
 	}
-	
+
 	public String toString(int start) {
 		return toString(start, arguments.size());
 	}
-	
+
 	public String toString(int start, int end) {
 		return toString(start, end, ArgumentSerializer.DEFAULT);
 	}
-	
+
 	public String toString(ArgumentSerializer serializer) {
 		return new ListArgument<>(arguments).toString(serializer);
 	}
-	
+
 	public String toString(int start, ArgumentSerializer serializer) {
 		return toString(start, serializer);
 	}
-	
+
 	public String toString(int start, int end, ArgumentSerializer serializer) {
 		return new ListArgument<>(arguments.subList(start, end)).toString(serializer);
 	}
-	
+
 	/**
 	 * Exception Construction
 	 */
-	
+
 	private IllegalArgumentException negativeOrZero() {
 		return new IllegalArgumentException("Bound must be positive!");
 	}
-	
+
 	private IndexOutOfBoundsException outOfBounce(int position) {
 		return new IndexOutOfBoundsException("Index: " + position + " - Size: " + count());
 	}
-	
+
 	private ObjectLockedException locked() {
 		return new ObjectLockedException("Cannot edit a locked object!");
 	}
-	
+
 	/*
 	 * 
-	 * 
+	 * Utils
 	 * 
 	 */
 
@@ -131,5 +132,13 @@ public class Arguments implements Iterable<BaseArgument> {
 	public Iterator<BaseArgument> iterator() {
 		return arguments.iterator();
 	}
-	
+
+	public Arguments copy(int start) {
+		return copy(start, arguments.size());
+	}
+
+	public Arguments copy(int start, int end) {
+		return new Arguments(arguments.subList(start, end));
+	}
+
 }
