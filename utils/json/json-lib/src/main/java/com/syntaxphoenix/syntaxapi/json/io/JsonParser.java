acce0e1c647d6a88bdf9c2aef5d143cdd3530a31
Laura Endreß <laura.endress@syntaxphoenix.com>
2021-04-15 09:05:09 +0200

    Bump up version and remove debug from jsonlib

--- utils/json/json-lib/src/main/java/com/syntaxphoenix/syntaxapi/json/io/JsonParser.java
+++ utils/json/json-lib/src/main/java/com/syntaxphoenix/syntaxapi/json/io/JsonParser.java
@@ -32,7 +32,6 @@ public class JsonParser implements TextDeserializer<JsonValue<?>> {
 
     protected JsonValue<?> read(JsonReader reader) throws IOException, JsonSyntaxException {
         JsonToken token = reader.next();
-        System.out.println(token.name());
         switch (token) {
         case NULL:
             reader.readNull();
@@ -47,7 +46,6 @@ public class JsonParser implements TextDeserializer<JsonValue<?>> {
             while (reader.hasNext()) {
                 array.add(read(reader));
             }
-            System.out.println("END_ARRAY");
             reader.endArray();
             return array;
         case START_OBJECT:
@@ -56,7 +54,6 @@ public class JsonParser implements TextDeserializer<JsonValue<?>> {
             while (reader.hasNext()) {
                 object.set(reader.readName(), read(reader));
             }
-            System.out.println("END_OBJECT");
             reader.endObject();
             return object;
         case BYTE:
